bf93ec16c9cb1e47864fbbdc65e244d463c622d4
corkrang <corkrang@gmail.com>
2013-07-02 20:04:51 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
@@ -10,11 +13,18 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      sign_in @user
   		flash[:success] = "Welcome to the Sample App!"
   		redirect_to @user
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+  end
+
+  def update
   end
 
   private
@@ -23,4 +33,11 @@ class UsersController < ApplicationController
   		params.require(:user).permit(:name, :email, :password,
   									 :password_confirmation)
   	end
+
+    # Before filters
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
